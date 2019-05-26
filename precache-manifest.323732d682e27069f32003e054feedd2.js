2d394c0ce59d154b0bf51c18efe6c7bb8fdb2991
AVAVT <itsavavt@gmail.com>
2019-05-27 05:33:27 +0700

    Updates

---
+++ precache-manifest.323732d682e27069f32003e054feedd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2959935209290cfe26fd9b5b49989f79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d726e2fbf23873c97861",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "43d76b47f0ab2f92e156",
+    "url": "/static/css/main.fa1a6b4b.chunk.css"
+  },
+  {
+    "revision": "d726e2fbf23873c97861",
+    "url": "/static/js/2.3533efda.chunk.js"
+  },
+  {
+    "revision": "43d76b47f0ab2f92e156",
+    "url": "/static/js/main.1831170c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
