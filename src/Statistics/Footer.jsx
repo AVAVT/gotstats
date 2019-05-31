22cc424b79647a941532274d4faaf9a4176ea44a
AVAVT <itsavavt@gmail.com>
2019-05-31 12:03:41 +0700

    Change repo path

--- src/Statistics/Footer.jsx
+++ src/Statistics/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
       <div className="row">
         <div className="col-sm-8">
           <p><em>"Got Stats?" analytics tool for <a target="_blank" rel="noopener noreferrer nofollow" href="https://www.online-go.com">OGS</a> by Chinitsu (aka AVAVT).</em></p>
-          <p><em>The source code is also available on <a href="https://github.com/AVAVT/g0tStats" target="_blank" rel="noopener noreferrer nofollow">Github</a>.</em></p>
+          <p><em>The source code is also available on <a href="https://github.com/AVAVT/gotstats" target="_blank" rel="noopener noreferrer nofollow">Github</a>.</em></p>
         </div>
         <div className="col-sm-4 text-right">
           <a href="http://avavt.github.io" target="_blank" rel="noopener noreferrer">About the author</a>
