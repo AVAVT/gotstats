26ec073ca24a4f44832913d7ae6eb653ec656ba0
AVAVT <itsavavt@gmail.com>
2026-04-16 22:08:00 +0700

    Revert "Updates"
    This reverts commit d1fe3e4f74dda6f82fcdfb8d2d9fd14194af5ebb.

---
+++ precache-manifest.5f993509f4653d6d40633ed81b5a36e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e40948d1bb9623c887165c45f5e36d",
+    "url": "/gotstats/index.html"
+  },
+  {
+    "revision": "4c41cad08fa847429029",
+    "url": "/gotstats/static/css/2.ef08faa8.chunk.css"
+  },
+  {
+    "revision": "6cad9524a1bbd4532014",
+    "url": "/gotstats/static/css/main.d08bb8e5.chunk.css"
+  },
+  {
+    "revision": "4c41cad08fa847429029",
+    "url": "/gotstats/static/js/2.137c0dc9.chunk.js"
+  },
+  {
+    "revision": "e869e60314af732ce71ef8625cd5ac82",
+    "url": "/gotstats/static/js/2.137c0dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cad9524a1bbd4532014",
+    "url": "/gotstats/static/js/main.5bc64035.chunk.js"
+  },
+  {
+    "revision": "c4482de5752fbd68368a",
+    "url": "/gotstats/static/js/runtime-main.6bfa2e2b.js"
+  }
+]);
